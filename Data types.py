810fb6bde3dc75827012ef44a7a27246bbcbb68f
ADDBITEZGOD <shuchita098@gmail.com>
2025-06-07 22:01:40 +0600

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,18 @@
+Name="Addirian"
+print("Name:", Name)
+print("Data type of the name is:",type(Name))
+Age=14
+print("Age:", Age)
+print("Data type of the Age is:",type(Age))
+School_Student=True
+print("School_Student:", School_Student)
+print("Data type of the School_Student is:",type(School_Student))
+Height=5.5
+print("Height:", Height)
+print("Data type of the Height is:",type(Height))
+Age=float(Age)
+print("Age:", Age)
+print("Data type of the Age is:",type(Age))
+Height=int(Height)
+print("Height:", Height)
+print("Data type of the Height is:",type(Height))
